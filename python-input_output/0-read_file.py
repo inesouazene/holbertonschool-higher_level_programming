434f66ba60b75ceb86a2775988e4dad9f477ed5f
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 12:20:24 +0200

    update task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,7 +3,6 @@
 
 
 def read_file(filename=""):
-    """Read a file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        for line in f:
-            print(f.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
