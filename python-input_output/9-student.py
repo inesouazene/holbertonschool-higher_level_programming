bf4e31170ee2bda7384ffde133524f0aada40c79
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 14:53:19 +0200

    add task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Class Student that defines a student"""
+
+
+class Student:
+    """
+    Defines a student by:
+    - first_name
+    - last_name
+    - age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return self.__dict__
