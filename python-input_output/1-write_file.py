fb52abe3b531dd55f8f35b3b0439977fa54f44d8
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 12:32:03 +0200

    fix pycodestyle error

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -12,5 +12,5 @@ def write_file(filename="", text=""):
     Returns:
         The number of characters written.
     """
-    with open (filename, encoding="utf-8") as f:
+    with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
