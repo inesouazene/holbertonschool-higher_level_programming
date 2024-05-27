9be2b78955a3cd1ec22a769231e9b4bac509af53
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 13:50:02 +0200

    add task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for class_to_json method."""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer, and boolean) for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        dict: A dictionary representation of the object's attributes.
+    """
+    return obj.__dict__
