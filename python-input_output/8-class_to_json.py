adbcbcf3359fc31da75dd29798f0bb64f32452dd
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 13:50:29 +0200

    fix pycodestyle error

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,7 +5,8 @@
 def class_to_json(obj):
     """
     Returns the dictionary description with simple data structure
-    (list, dictionary, string, integer, and boolean) for JSON serialization of an object.
+    (list, dictionary, string, integer, and boolean)
+    for JSON serialization of an object.
 
     Args:
         obj: An instance of a Class.
