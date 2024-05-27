ca165f51eab885921497837e05f818954468a919
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 17:57:31 +0200

    update task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,4 +13,4 @@ def save_to_json_file(my_obj, filename):
     filename (str): The name of the file to write to.
     """
     with open(filename, mode="w") as file:
-        json.dumps(my_obj)
+        json.dump(my_obj, file)
