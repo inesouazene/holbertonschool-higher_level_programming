335443d6eaff73703d5c070bc057039a3666c72e
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 12:57:50 +0200

    remove encoding from task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     my_obj (any): The object to be serialized and written to the file.
     filename (str): The name of the file to write to.
     """
-    with open(filename, mode='w', encoding='utf-8') as file:
+    with open(filename, mode='w') as file:
         json.dump(my_obj, file)
