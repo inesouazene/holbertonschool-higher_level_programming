8fb776fb01e8dbd0625c96b22b47056623a26d6a
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-27 13:08:32 +0200

    add task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a JSON file-reading function."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Reads a JSON file and returns the corresponding Python object.
+
+    Args:
+        filename (str): The name of the JSON file to read.
+
+    Returns:
+        object: The Python object representation of the JSON data.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        json.JSONDecodeError: If the file contains invalid JSON.
+    """
+    with open(filename, mode='r', encoding="utf-8") as file:
+        return json.load(file)
