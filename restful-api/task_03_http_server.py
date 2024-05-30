4fec9d40cfea61dc6dd388bf9856b75b37d9c906
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-05-30 16:02:14 +0200

    update task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -28,17 +28,18 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"OK")
         elif self.path == "/info":
             self.send_response(200)  # HTTP status 200 OK
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            response = {
+            data = {
                 "version": "1.0",
                 "description": "A simple API built with http.server",
             }
+            self.wfile.write(json.dumps(data).encode("utf-8"))
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
