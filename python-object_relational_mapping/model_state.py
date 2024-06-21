86d2297893c8d0edf4f5cedb295c13e8690def61
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-06-21 22:55:09 +0200

    update task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""Nameless module to declare tables for db hbtn_0e_6_usa
 """
-import sys
-from model_state import Base, State
 
-from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """Class to declare the states database table"""
+
+    __tablename__ = "states"
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
